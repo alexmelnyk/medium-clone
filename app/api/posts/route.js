766d6c4493740223c53b363c329caea9e8d82a37
Oleksii Melnyk <oleksii@Oleksiis-MacBook-Pro.local>
2024-02-28 11:40:01 +0200

    create post API

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
+import { Draft07 } from "json-schema-library";
 import prisma from "@/lib/prisma";
+import PostSchema from "@/schemas/post.schema.json";
 
 export async function GET(request) {
   try {
@@ -13,12 +15,44 @@ export async function GET(request) {
         include: {
           user: true,
         },
+        orderBy: {
+          createdAt: "desc",
+        },
       }),
       prisma.Post.count(),
     ]);
-    const pagesCount = Math.floor(postsCount / take);
+    const pagesCount = Math.ceil(postsCount / take);
 
     return NextResponse.json({ posts, pagesCount }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+    const jsonSchema = new Draft07(PostSchema);
+    const errors = jsonSchema.validate(body);
+
+    if (errors.length) {
+      return NextResponse.json({ errors }, { status: 400 });
+    }
+
+    const user = await prisma.User.findUniqueOrThrow({
+      where: {
+        id: body.userId,
+      },
+    });
+
+    const post = await prisma.Post.create({
+      data: {
+        ...body,
+        userId: user.id,
+      },
+    });
+
+    return NextResponse.json({ post }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
